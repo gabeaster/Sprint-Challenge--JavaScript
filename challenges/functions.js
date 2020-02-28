1ddcbcbe60c293bd1ca7c103055df42fdbbfdf4f
gabeaster <gab.easter@live.com>
2020-02-28 13:16:48 -0500

    prototypes done, moving on to classes

--- challenges/functions.js
+++ challenges/functions.js
@@ -43,7 +43,7 @@ console.log(greeting('Gabby', 'Easter'));
 console.log(consume(2, 2, add)); // 4
 console.log(consume(10, 16, multiply)); // 160
 console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
-CORRECT
+
 
 // ==== Closures ==== [done]
 
