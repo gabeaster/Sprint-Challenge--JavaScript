c4ce376446ede1b56d541cf2079aa6af1dbea81e
gabeaster <gab.easter@live.com>
2020-02-28 12:04:28 -0500

    task 1 request 1 and request 1 complete

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -8,27 +8,54 @@
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
 
+let  trex= {
+  name: 'tyrannosaurus',
+  diet: 'carnivorous',
+  weight: '7000kg',
+  length: '12m',
+  period: 'Late Cretaceous',
+  roar: function(){
+    return `RAWERSRARARWERSARARARRRR!`
+  }
+};
+
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+
+let  stegos= {
+  name: 'stegosaurus',
+  diet: 'herbivorous',
+  weight: '2000kg',
+  length: '9m',
+  period: 'Late Jurassic'
+}
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
 
+let raptor = {
+  name: 'velociraptor',
+  diet: 'carnivorous',
+  weight: '15kg',
+  length: '1.8m',
+  period: 'Late Cretaceous'
+}
+
 // Using your dinosaur objects, log answers to these questions:
 
-// How much did tyrannosaurus weigh?
-console.log();
+// How much did tyrannosaurus weigh? 
+console.log(trex.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(raptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegos.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(trex.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(trex.roar);
 
 
 // ==== Arrays ====
@@ -49,18 +76,31 @@ const graduates = [
 ];
 
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
-
+DONE- LOG CORRECT "(10) ["Coastal Carolina University", "Fachhochschule Rosenheim, Hochschule für Technik und Wirtschaft", "International Medical & Technological University", "Marian College", "Missouri Southern State College", "Salem University", "Sultan Salahuddin Abdul Aziz Shah Polytechnic", "The School of the Art Institute of Chicago", "Universidad Católica de Ávila", "Universitat Rovira I Virgili Tarragona"]"
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
+
+for (let i = 0; i < graduates.length; i++){
+  universities.push(graduates[i].university)
+}
+universities.sort();
+
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
 
 The resulting contact information strings should have a space between the first name and the email, like this: 
 "Josh josh@example.com"
+DONE - LOG CORRECT "(10) ["Cynde ctorry0@macromedia.com", "Saundra swhal1@state.gov", "Lambert lparham2@techcrunch.com", "Modestine mdolder3@symantec.com", "Chick camorts4@google.com.au", "Jakob jharken5@spiegel.de", "Robbi rbrister6@redcross.org", "Colline cbrosh7@alibaba.com", "Michail mrome8@shinystat.com", "Hube hlethbrig9@foxnews.com"]"
 
 Log the result of your new array. */
+
 const contactInfo = [];
+
+for (let i = 0; i < graduates.length; i++){
+  contactInfo.push(graduates[i].first_name + " " + graduates[i].email)
+}
+
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
